bbe6ad365502b599eed2926421316465c952e6c9
Garth-42 <12802614+Garth-42@users.noreply.github.com>
2018-07-01 14:00:39 -0500

    Initial Commit
    Hello world!

---
+++ linkage_calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+I created the first version of this software when I was a student at Iowa State University.
+
+Unfortunately, I accidentally deleted all of my versions of it when I was trying to hack together a pyhton script to backup my files... Oh the irony.
+
+Welp, here we go.
+
+I need a good project to start learning python and I want to rewrite this anyway. The code I wrote earlier was a complete mess.
+"""
+
+
+# Eventually use pipenv, git, spinx, and pytest, typing for this project in order to get familiar with proper coding documentation for larger projects
+
+import numpy as np
---
+++ testing.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 28 22:19:42 2018
+
+@author: garth
+"""
+
+import untitles
+
+print( untitles.variable )
