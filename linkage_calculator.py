a043fcfde4a7d289685480a313a61ff822a9e9f0
Garth-42 <12802614+Garth-42@users.noreply.github.com>
2018-07-02 00:45:23 -0500

    Created datafame object
    Troubleshooted the overall structure of the dataframe object that will be used for the calculations. Easily extensible for new quantities that will want to be calculated for each angle the linkage will go through.

--- linkage_calculator.py
+++ linkage_calculator.py
@@ -11,6 +11,34 @@ I need a good project to start learning python and I want to rewrite this anyway
 """
 
 
-# Eventually use pipenv, git, spinx, and pytest, typing for this project in order to get familiar with proper coding documentation for larger projects
+#jupyter and ipython version?
 
+# Eventually use pipenv, git, spinx, and pytest, typing for this project in order to get familiar with proper coding documentation for larger proj
+import math
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+'''
+d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+     'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
+data_frame = pd.DataFrame(d)
+'''
+
+calc_interval = list(range(0,360)) #0 to 359 with 360 = 0
+#make this an int and do in radians...multiply by the decimal of accuracy want to get?
+"""
+d = {'link 1' : pd.Series(np.zeros(360) ), index = calc_interval[]),
+     'link 2' : pd.Series(np.zeros(360) ), index = calc_interval[]),
+     'link 3' : pd.Series(np.zeros(360) ), index = calc_interva[]),
+     'link 4' : pd.Series(np.zeros(360) ), index = calc_interval[])}
+data_frame = pd.DataFrame(d)
+"""
+this_is_array = np.full(shape=(360),fill_value=np.nan,dtype=np.float)
+
+d = {'one' : pd.Series(this_is_array, index=calc_interval[:]),
+     'two' : pd.Series(list(range(360)), index=calc_interval[:])}
+data_frame = pd.DataFrame(d)
+data_frame.index.names = ['Angle']
+
